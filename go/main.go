eb1b0d7266f7aa2d35740df22f4827d473029923
mdwitr0 <mdwit0r@gmail.com>
2022-10-05 19:18:00 +0200

    init go server

---
+++ go/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/valyala/fasthttp"
+)
+
+var (
+	port               = "3008"
+	strContentType     = []byte("Content-Type")
+	strApplicationJSON = []byte("application/json")
+	httpClient         *fasthttp.Client
+)
+
+func requestHandler(ctx *fasthttp.RequestCtx) {
+	if string(ctx.Method()) == "GET" {
+		switch string(ctx.Path()) {
+		default:
+			ctx.Response.Header.SetCanonical(strContentType, strApplicationJSON)
+			ctx.Response.SetStatusCode(200)
+
+			response := map[string]string{"result": fmt.Sprintf("hello world")}
+			if err := json.NewEncoder(ctx).Encode(response); err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
+func main() {
+	fmt.Printf("HTTP interface listen port: %s\n", port)
+	h := requestHandler
+	h = fasthttp.CompressHandler(h)
+	httpClient = &fasthttp.Client{
+		MaxConnsPerHost: 2048,
+	}
+	if err := fasthttp.ListenAndServe(":"+port, h); err != nil {
+		log.Fatalf("Error in ListenAndServe: %s", err)
+	}
+}
