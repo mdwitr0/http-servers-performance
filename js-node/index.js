caf17a669c4b7c0f6ac0b5f84c2dc574817e6aa9
mdwitr0 <mdwit0r@gmail.com>
2022-10-25 19:08:05 +0200

    update rocket config

--- js-node/index.js
+++ js-node/index.js
@@ -1,34 +1,14 @@
 import cluster from 'node:cluster';
 import http from 'node:http';
 import { cpus } from 'node:os';
-import process from 'node:process';
 
-const numCPUs = cpus().length;
+const PORT = 3010;
 
-if (cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-
-    // Fork workers.
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
-} else {
-    // Workers can share any TCP connection
-    // In this case it is an HTTP server
-    const PORT = 3010;
-
-    const requestListener = function (req, res) {
-        res.writeHead(200);
-        res.end("Hello world");
-    }
-
-    const server = http.createServer(requestListener);
-    console.log(`HTTP interface listen port: ${PORT}`)
-    server.listen(PORT);
-
-    console.log(`Worker ${process.pid} started`);
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("Hello world");
 }
+
+const server = http.createServer(requestListener);
+console.log(`HTTP interface listen port: ${PORT}`)
+server.listen(PORT);
