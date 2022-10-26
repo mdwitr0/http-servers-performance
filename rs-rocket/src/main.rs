6927fbb2bfcaf7a0302b3bb9f4bc4f8f728b85b3
mdwitr0 <mdwit0r@gmail.com>
2022-10-26 20:59:53 +0200

    fix rocket

--- rs-rocket/src/main.rs
+++ rs-rocket/src/main.rs
@@ -1,34 +1,11 @@
-extern crate env_logger;
-extern crate futures;
-extern crate tokio_minihttp;
-extern crate tokio_proto;
-extern crate tokio_service;
+#[macro_use] extern crate rocket;
 
-use std::io;
-
-use futures::future;
-use tokio_minihttp::{Request, Response, Http};
-use tokio_proto::TcpServer;
-use tokio_service::Service;
-
-struct HelloWorld;
-
-impl Service for HelloWorld {
-    type Request = Request;
-    type Response = Response;
-    type Error = io::Error;
-    type Future = future::Ok<Response, io::Error>;
-
-    fn call(&self, _request: Request) -> Self::Future {
-        let mut resp = Response::new();
-        resp.body("Hello, world!");
-        future::ok(resp)
-    }
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
-fn main() {
-    drop(env_logger::init());
-    let addr = "127.0.0.1:3014".parse().unwrap();
-    TcpServer::new(Http, addr)
-        .serve(|| Ok(HelloWorld));
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
